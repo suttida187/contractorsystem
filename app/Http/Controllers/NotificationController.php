3736366afe9442f22d2d44be09969947ac639fe3
THANETNove <novel2020manga@gmail.com>
2025-02-11 19:14:06 +0700

    add

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -14,8 +14,8 @@ class NotificationController extends Controller
     public function fetch()
     {
 
-     /*    if (Auth::user()->role == 'sale') { */
-            $notifications = DB::table('notifications')
+        if (Auth::user()->role == 'admin') {
+            $notifications = DB::table('notifications_admins')
                 ->whereNull('read_at')
                 ->latest()
                 ->get()
@@ -27,7 +27,50 @@ class NotificationController extends Controller
                         'url' => route('notifications.markAsRead', $notification->id),
                     ];
                 });
-       /*  } */
+        }
+        if (Auth::user()->role == 'sale') {
+            $notifications = DB::table('notifications_sales')
+                ->whereNull('read_at')
+                ->latest()
+                ->get()
+                ->map(function ($notification) {
+                    return [
+                        'id' => $notification->id,
+                        'message' => json_decode($notification->data, true)['message'],
+                        'time' => Carbon::parse($notification->created_at)->diffForHumans(),
+                        'url' => route('notifications.markAsRead', $notification->id),
+                    ];
+                });
+        }
+        if (Auth::user()->role == 'pm') {
+            $notifications = DB::table('notifications_pms')
+                ->whereNull('read_at')
+                ->latest()
+                ->get()
+                ->map(function ($notification) {
+                    return [
+                        'id' => $notification->id,
+                        'message' => json_decode($notification->data, true)['message'],
+                        'time' => Carbon::parse($notification->created_at)->diffForHumans(),
+                        'url' => route('notifications.markAsRead', $notification->id),
+                    ];
+                });
+        }
+
+        if (Auth::user()->role == 'contractor') {
+            $notifications = DB::table('notifications_contractors')
+                ->whereNull('read_at')
+                ->latest()
+                ->get()
+                ->map(function ($notification) {
+                    return [
+                        'id' => $notification->id,
+                        'message' => json_decode($notification->data, true)['message'],
+                        'time' => Carbon::parse($notification->created_at)->diffForHumans(),
+                        'url' => route('notifications.markAsRead', $notification->id),
+                    ];
+                });
+        }
 
 
 
