4323f385bc1313e25d9b37cc5530a9594512f7f0
THANETNove <novel2020manga@gmail.com>
2025-02-24 20:06:53 +0700

    add

--- resources/views/layouts/stepStatusNull.blade.php
+++ resources/views/layouts/stepStatusNull.blade.php
@@ -1,1 +1,3 @@
-<div class="progress-container-status" id="progress-container"></div>
+<div class="ml-progress-container-status">
+    <div class="progress-container-status" id="progress-container"></div>
+</div>
